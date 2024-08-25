5c8bbe603185fd1862f9c853c0299145be46fbf8
Thành <huuthanh9agm@gmail.com>
2024-08-25 23:50:07 +0700

    done-header

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+function load(selector, path) {
+    const cached = localStorage.getItem(path);
+    if (cached) {
+        $(selector).innerHTML = cached;
+    }
+
+    fetch(path)
+        .then((res) => res.text())
+        .then((html) => {
+            if (html !== cached) {
+                $(selector).innerHTML = html;
+                localStorage.setItem(path, html);
+            }
+        })
+        .finally(() => {
+            window.dispatchEvent(new Event("template-loaded"));
+        });
+}
+
+// Thêm code sau vào CUỐI file /assets/js/scripts.js:
+
+/**
+ * JS toggle
+ *
+ * Cách dùng:
+ * <button class="js-toggle" toggle-target="#box">Click</button>
+ * <div id="box">Content show/hide</div>
+ */
+window.addEventListener("template-loaded", initJsToggle);
+
+function initJsToggle() {
+    $$(".js-toggle").forEach((button) => {
+        const target = button.getAttribute("toggle-target");
+        if (!target) {
+            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+        }
+        button.onclick = () => {
+            if (!$(target)) {
+                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+            }
+            const isHidden = $(target).classList.contains("hide");
+
+            requestAnimationFrame(() => {
+                $(target).classList.toggle("hide", !isHidden);
+                $(target).classList.toggle("show", isHidden);
+            });
+        };
+    });
+}
